d49faa644e8e9b6f670ea42b5f10dbfcd9acd543
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2018-10-20 23:11:32 +0800

    init

---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = props => {
+  return 'title'
+}
+
+Title.propTypes = {}
+
+export default Title
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  mode: 'development',
+}
