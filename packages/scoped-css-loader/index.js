614ed58c0c293b5972f033f1cb13d68172e05290
Liang <gaoxiaoliangz@gmail.com>
2018-10-24 10:30:37 +0800

    -

--- packages/scoped-css-loader/index.js
+++ packages/scoped-css-loader/index.js
@@ -5,7 +5,6 @@ const qs = require('qs')
 module.exports = function(source) {
   // @ts-ignore
   const resourceQuery = qs.parse(this.resource.split('?')[1])
-
   if (resourceQuery.scopeId) {
     const { code, map, errors } = compileStyle({
       source,
