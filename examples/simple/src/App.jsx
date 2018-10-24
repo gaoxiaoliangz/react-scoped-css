b4ac320b10f1691f6b58e7535a441186c4ed3185
Liang <gaoxiaoliangz@gmail.com>
2018-10-24 10:21:51 +0800

    -

--- examples/simple/src/App.jsx
+++ examples/simple/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="app">
-      <Header title="React scoped CSS">This is a test</Header>
+      <Header title="React scoped CSS">header content</Header>
       <p>content color should be #666</p>
     </div>
   )
