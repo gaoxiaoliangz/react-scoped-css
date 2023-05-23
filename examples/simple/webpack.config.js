6053df940d23a1a93a378bdbe3236172dfcbe656
Hugo Sales <hugo@hsal.es>
2023-05-23 21:51:46 +0100

    Fix webpack dev server 'Cannot GET /'

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
   },
   mode: 'development',
   devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/')
+    },
+  },
   module: {
     rules: [
       {
