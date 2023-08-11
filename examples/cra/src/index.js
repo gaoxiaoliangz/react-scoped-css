23d144f312d91e53e2c8a0e4a8191404b59cf637
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2023-08-11 22:08:51 +0800

    chore: upgrade cra example deps

--- examples/cra/src/index.js
+++ examples/cra/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
